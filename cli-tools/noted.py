6f11b97bb8f0a0d87565212a5ec50ebfaf325dba
defrindr <defrindr@gmail.com>
2020-05-01 09:22:19 +0700

    clean up

--- cli-tools/noted.py
+++ cli-tools/noted.py
@@ -4,60 +4,77 @@ import json
 from base64 import b64decode as decode,b64encode as encode
 from colorama import init,Fore
 
-settings = []
 init(autoreset=True)
 
-with open('config.json','r') as files:
-    settings = json.loads(''.join(files.readlines()))
 
-def getJson():
-    req = requests.get('https://api.github.com/repos/'+settings['GITHUB_USERNAME']+'/noted/contents/assets/noted/source.json')
-    return req.json()
+class noted:
+    def __init__(self):
+        self.settings = self.getSettings()
+        self.link = 'https://api.github.com/repos/'+self.settings['GITHUB_USERNAME']+'/noted/contents/assets/noted/source.json'
+        self.today = self.getDateToday()
+        self.sha,self.content = self.parseJson()
 
-jsonData = getJson()
+    def getSettings(self):
+        with open('config.json','r') as files:
+            return json.loads(''.join(files.readlines()))
 
-sha = jsonData["sha"]
-content = json.loads(decode(jsonData["content"]).decode())
+    def getJson(self):
+        req = requests.get(self.link)
+        return req.json()
 
-today = date.today()
-today = today.strftime("%Y-%m-%d");
+    def parseJson(self):
+        jsonData = self.getJson()
+        sha,content = jsonData["sha"],json.loads(decode(jsonData["content"]).decode())
+        return [sha,content]
 
-newPost = []
-print(Fore.BLUE+"[*] Jika sudah tekan CTRL+D untuk mengirim .")
+    def getDateToday(self):
+        today = date.today().strftime("%Y-%m-%d");
+        return today
 
-while True:
-    try:
-        line = input()
-    except EOFError:
-        break
-    newPost.append(line)
+    def getInputuser(self):
+        user_input = []
+        print(Fore.BLUE+"[*] Jika sudah tekan CTRL+D untuk mengirim .")
+        while True:
+            try:
+                line = input()
+            except EOFError:
+                break
+            user_input.append(line)
+        return '\n'.join(user_input)
 
-newPost = '\n'.join(newPost);
-content.append({
-    "date": today,
-    "post": newPost
-})
+    def prepareUpdate(self,user_input):
+        self.content.append({
+            "date": self.today,
+            "post": user_input
+        })
+        payloads = {
+            "path": "assets/noted/source.json",
+            "message": "Update "+self.today,
+            "content": encode(json.dumps(self.content).encode()).decode()+"\n",
+            "branch": "master",
+            "sha": self.sha
+        }
+        headers = {
+            "Authorization": "token "+self.settings['SECRET_TOKEN']
+        }
+        return [payloads,headers]
 
-payloads = {
-    "path": "assets/noted/source.json",
-    "message": "Update "+today,
-    "content": encode(json.dumps(content).encode()).decode()+"\n",
-    "branch": "master",
-    "sha": sha
-}
+    def question2Continue(self,headers,payloads):
+        confirm = input(Fore.YELLOW+'[?] Apakah Anda Yakin [y/n] ? ')
+        if confirm.lower() == "y":
+            res = requests.put(self.link,headers=headers,json=payloads)
+            res = res.json()
+            if res['content']:
+                print(Fore.GREEN+"[+] Berhasil meng-update note.")
+        else:
+            print(Fore.RED+"[+] exiting ....")
+            exit()
 
-headers = {
-    "Authorization": "token "+settings['SECRET_TOKEN']
-}
+    def main(self):
+        inputUser = self.getInputuser()
+        payloads,headers = self.prepareUpdate(inputUser)
+        self.question2Continue(headers,payloads)
 
-confirm = input(Fore.YELLOW+'[?] Apakah Anda Yakin [y/n] ? ')
+noted().main()
 
-if confirm.lower() == "y":
-    res = requests.put('https://api.github.com/repos/'+settings['GITHUB_USERNAME']+'/noted/contents/assets/noted/source.json',headers=headers,json=payloads)
-    res = res.json()
-    if res['content']:
-        print(Fore.GREEN+"[+] Berhasil meng-update note.")
-else:
-    print(Fore.RED+"[+] exiting ....")
-    exit()
 
